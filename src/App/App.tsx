a6c58803dc04ddf99e8e38f44ceeacf891d4234e
yeongjong310 <tohero21@naver.com>
2021-10-31 21:25:14 +0900

    fix: 변수 못불러오는 문제 일시적 해결

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,47 @@
 import { dom } from '@/utils/babel';
-import { Node } from '@/components';
+import { Node, Header } from '@/components';
 import styles from '../styles/normalize.module.scss';
+import { Landing } from '@/pages';
 
 interface State {
-  displayTest: boolean;
+  currentPage: string;
+  pages: { id: string; to: string; text: string }[];
 }
 
 /** @jsx dom */
 export default class App extends Node<unknown, State> {
-  constructor() {
-    super();
-    this.state = {
-      displayTest: true,
-    };
-  }
+  state = {
+    currentPage: 'landing',
+    pages: [
+      { id: 'landing', to: '/', text: '홈' },
+      { id: 'about', to: '/about', text: '소개' },
+      { id: 'service', to: '/service', text: '서비스' },
+      { id: 'contact', to: '/customer', text: '고객센터' },
+    ],
+  };
 
-  onClick() {
-    this.setState({ displayTest: !this.state.displayTest });
+  renderPage() {
+    switch (this.state.currentPage) {
+      case 'landing':
+        return <Landing />;
+      // case 'about':
+      //   return <About />;
+      // case 'service':
+      //   return <Service />;
+      // case 'contact':
+      //   return <Contact />;
+      default:
+        return <Landing />;
+    }
   }
 
   template() {
-    return <div class={styles.global}></div>;
+    return (
+      <div>
+        <Header></Header>
+        <main>{this.renderPage()}</main>
+        {/* <Footer></Footer> */}
+      </div>
+    );
   }
 }
