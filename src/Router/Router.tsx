c5e722a7c6fa990378e6e413ae5b58a5cc9ac79a
yeongjong310 <tohero21@naver.com>
2021-11-04 20:00:31 +0900

    Router 설계 완료

---
+++ src/Router/Router.tsx
@@ -0,0 +1,49 @@
+import { dom } from '@/utils/babel';
+import { Node } from '@/components';
+import { RouterContext } from '@/GlobalState/GlobalState';
+import { About, Landing } from '@/pages';
+
+/** @jsx dom */
+export class Router extends Node<unknown, { type: boolean }> {
+  constructor(props: unknown) {
+    super(props);
+    RouterContext.subscribe(() => this.reRender());
+  }
+  template() {
+    return (
+      <DocumentFragment>
+        <Route route="/" component={Landing} />
+        <Route route="/about" component={About} />
+        <Route route="/service" component={About} />
+        <Route route="/map" component={About} />
+      </DocumentFragment>
+    );
+  }
+}
+
+interface RouteProps {
+  route: string;
+  component: any;
+}
+
+export class Route extends Node<RouteProps> {
+  template() {
+    const { route } = this.props;
+    const Component = this.props.component;
+
+    return location.pathname === route && <Component />;
+  }
+}
+
+// interface RedirectProps {
+//   component: any;
+// }
+
+// export class Redirect extends Node<RouteProps> {
+//   template() {
+//     const { route } = this.props;
+//     const Component = this.props.component;
+
+//     return <Component />;
+//   }
+// }
