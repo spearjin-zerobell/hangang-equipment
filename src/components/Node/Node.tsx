a0e10e056aefb765523cc4cf0273ae14267db870
yeongjong310 <tohero21@naver.com>
2021-11-07 19:09:38 +0900

    feat: 공통 Props 추가, fragment 로직 수정
    기존에 자식 1개를 취득해서 부모를 찾아 해당 자식만 교채하는 방식은 나머지 자식들을 교채하지 못하기 때문에 문제가 발생했다.
    
    이 부분을 해결하기 위해 배열 형태로 로직을 변경하였다.

--- src/components/Node/Node.tsx
+++ src/components/Node/Node.tsx
@@ -4,11 +4,13 @@ interface commonProps {
   children?: HTMLElement[] | HTMLElement | ((...pram: unknown[]) => HTMLElement);
   class?: string;
   role?: string;
+  tabindex?: string;
+  onclick?: (e: Event) => void;
 }
 
 /** @jsx dom */
 export default class Node<Props = unknown, State = unknown> {
-  $node: ChildNode;
+  $node: ChildNode | ChildNode[];
   props?: Props & commonProps;
   state?: State;
 
@@ -22,27 +24,36 @@ export default class Node<Props = unknown, State = unknown> {
   }
 
   reRender() {
-    const $parent = this.$node.parentElement;
+    const $parent = Array.isArray(this.$node) ? this.$node[0].parentNode : this.$node.parentNode;
     const $newNode = this.template();
 
     // document fragment는 replaceChild에 의해 자식이 모두 사라지기 때문에
     // this.$node를 자식으로 갱신하려면 그 전에 변수에 저장해 놓아야 한다.
-    let $contents;
-    if ($newNode.nodeName === '#document-fragment') {
-      $contents = $newNode.childNodes[0];
+    let $contents: ChildNode | ChildNode[];
+    if ($newNode?.nodeName === '#document-fragment') {
+      $contents = Array.from($newNode.children);
     } else {
       $contents = $newNode;
     }
 
-    $parent.replaceChild($newNode, this.$node);
+    if (Array.isArray($contents) && Array.isArray(this.$node)) {
+      for (let i = 0; i < $contents.length; i++) {
+        $parent.replaceChild($contents[i], this.$node[i]);
+      }
+    } else {
+      const $newNode = $contents as ChildNode;
+      $parent.replaceChild(this.$node as ChildNode, $newNode);
+    }
 
     this.$node = $contents;
   }
 
   render() {
     const $newNode = this.template();
-    if ($newNode.nodeName === '#document-fragment') this.$node = $newNode.childNodes[0];
-    else this.$node = $newNode;
+    if ($newNode.nodeName === '#document-fragment') {
+      // console.log($newNode.children); // undefined가 뜨는 경우 처리 필요
+      this.$node = Array.from($newNode.children);
+    } else this.$node = $newNode;
     return $newNode;
   }
 
