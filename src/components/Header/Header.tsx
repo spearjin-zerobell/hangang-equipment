151e07a9d2221de768bda1835c3d2b5647c6c610
yeongjong310 <tohero21@naver.com>
2021-11-07 19:14:57 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import { dom } from '@/utils/babel';
+import { Link, Node, Nav } from '@/components';
+import style from './Header.module.scss';
+import logoSrc from './assets/img/logo.png';
+
+/** @jsx dom */
+export default class Header extends Node<unknown, { isOpenMenu: boolean }> {
+  state = {
+    isOpenMenu: true,
+  };
+
+  handleToggleMenu(e: Event) {
+    const $menuBtn = e.target as HTMLElement;
+    const $nav = $menuBtn.closest('header').querySelector('[class*=nav]') as HTMLElement;
+
+    $menuBtn.classList.toggle(style.btnOpen);
+    $nav.classList.toggle(style.navOpen);
+
+    if ($nav.classList.contains(style.navOpen)) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  template() {
+    return (
+      <header>
+        <div class={style.header}>
+          <Link class={style.logo} to="/">
+            <img src={logoSrc} />
+          </Link>
+          <div
+            class={style.menuToggle}
+            onclick={this.handleToggleMenu.bind(this)}
+            role="button"
+            aria-controls="menu"
+            aria-haspopup="menu"
+            aria-expanded="false"
+          >
+            <span role="presentation"></span>
+          </div>
+        </div>
+        <Nav />
+      </header>
+    );
+  }
+}
