718da7118eb72e081f9878858416d46a35a8d23d
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-11-08 15:12:48 +0900

    feat: 서비스에 mock 구현

---
+++ src/pages/Service/ServiceData.tsx
@@ -0,0 +1,105 @@
+import pipeProcess1 from './assets/img/process1.jpeg';
+import pipeProcess2 from './assets/img/process2.jpeg';
+import pipeProcess3 from './assets/img/process3.jpeg';
+import pipeProcess4 from './assets/img/process4.jpeg';
+import pipeProcess5 from './assets/img/process5.jpeg';
+import pipeProcess6 from './assets/img/process6.jpeg';
+
+import icon1 from './assets/icon/service1.svg';
+import icon2 from './assets/icon/service2.svg';
+import icon3 from './assets/icon/service3.svg';
+import icon4 from './assets/icon/service4.svg';
+
+import before1 from './assets/img/before1.jpeg';
+import after1 from './assets/img/after1.jpeg';
+
+export const pipeInfo = [
+  { title: '배관설비', icon: icon1 },
+  {
+    title: '01. 현장',
+    img: pipeProcess1,
+  },
+  {
+    title: '02. 준비',
+    img: pipeProcess2,
+  },
+  {
+    title: '03. 철거',
+    img: pipeProcess3,
+  },
+  {
+    title: '04. 배관교체',
+    img: pipeProcess4,
+  },
+  {
+    title: '05. 미장',
+    img: pipeProcess5,
+  },
+  {
+    title: '06. 완료',
+    img: pipeProcess6,
+  },
+];
+
+export const heatingInfo = [
+  { title: '난방설비', icon: icon2 },
+  {
+    title: '현장2',
+    img: pipeProcess1,
+  },
+  {
+    title: '준비2',
+    img: pipeProcess2,
+  },
+  {
+    title: '철거2',
+    img: pipeProcess3,
+  },
+  {
+    title: '배관교체2',
+    img: pipeProcess4,
+  },
+  {
+    title: '미장2',
+    img: pipeProcess5,
+  },
+  {
+    title: '완료2',
+    img: pipeProcess6,
+  },
+];
+
+export const leakingInfo = [
+  { title: '누수설비', icon: icon3 },
+  {
+    title: '시공전',
+    img: before1,
+  },
+  {
+    title: '시공후',
+    img: after1,
+  },
+];
+
+export const repairsInfo = [
+  { title: '집수리', icon: icon4 },
+  {
+    title: '시공전',
+    img: before1,
+  },
+  {
+    title: '시공후',
+    img: after1,
+  },
+];
+
+export const questionInfo = [
+  {
+    question: '배관설비가 무엇인가요?',
+    answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+  },
+  {
+    question: '비용은 어떻게 되나요?',
+    answer: '방문후 정확한 견적이 가능합니다.',
+  },
+];
