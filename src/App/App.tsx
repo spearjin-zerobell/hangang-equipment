67b4bf6d656b29ec3963655cba2fa28145708512
yeongjong310 <tohero21@naver.com>
2021-11-08 17:57:28 +0900

    feat: app 컴포넌트의 필요없는 state 제거

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -6,13 +6,8 @@ import 'element-closest-polyfill';
 import '@/styles/normalize.css';
 import '@/styles/global.css';
 
-interface State {
-  currentPage: string;
-  pages: { id: string; to: string; text: string }[];
-}
-
 /** @jsx dom */
-export default class App extends Node<unknown, State> {
+export default class App extends Node {
   template() {
     return (
       <fragment>
