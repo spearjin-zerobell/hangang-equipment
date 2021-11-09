7dc2b51e4b5de5c57d3a742d4bc9e041e0ef4f1a
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-11-09 16:04:13 +0900

    fix: 서비스 데이터 구조 수정

--- src/pages/Service/ServiceData.tsx
+++ src/pages/Service/ServiceData.tsx
@@ -13,93 +13,152 @@ import icon4 from './assets/icon/service4.svg';
 import before1 from './assets/img/before1.jpeg';
 import after1 from './assets/img/after1.jpeg';
 
-export const pipeInfo = [
-  { title: '배관설비', icon: icon1 },
-  {
-    title: '01. 현장',
-    img: pipeProcess1,
+export const pipeInfo = {
+  title: {
+    name: '배관설비',
+    icon: icon1,
   },
-  {
-    title: '02. 준비',
-    img: pipeProcess2,
-  },
-  {
-    title: '03. 철거',
-    img: pipeProcess3,
-  },
-  {
-    title: '04. 배관교체',
-    img: pipeProcess4,
-  },
-  {
-    title: '05. 미장',
-    img: pipeProcess5,
-  },
-  {
-    title: '06. 완료',
-    img: pipeProcess6,
-  },
-];
 
-export const heatingInfo = [
-  { title: '난방설비', icon: icon2 },
-  {
-    title: '현장2',
-    img: pipeProcess1,
-  },
-  {
-    title: '준비2',
-    img: pipeProcess2,
-  },
-  {
-    title: '철거2',
-    img: pipeProcess3,
-  },
-  {
-    title: '배관교체2',
-    img: pipeProcess4,
-  },
-  {
-    title: '미장2',
-    img: pipeProcess5,
-  },
-  {
-    title: '완료2',
-    img: pipeProcess6,
-  },
-];
+  content: [
+    {
+      name: '0.1 현장',
+      img: pipeProcess1,
+    },
+    {
+      name: '02. 준비',
+      img: pipeProcess2,
+    },
+    {
+      name: '03. 철거',
+      img: pipeProcess3,
+    },
+    {
+      name: '04. 배관교체',
+      img: pipeProcess4,
+    },
+    {
+      name: '05. 미장',
+      img: pipeProcess5,
+    },
+    {
+      name: '06. 완료',
+      img: pipeProcess6,
+    },
+  ],
+};
 
-export const leakingInfo = [
-  { title: '누수설비', icon: icon3 },
-  {
-    title: '시공전',
-    img: before1,
+export const heatingInfo = {
+  title: {
+    name: '난방설비',
+    icon: icon2,
   },
-  {
-    title: '시공후',
-    img: after1,
-  },
-];
 
-export const repairsInfo = [
-  { title: '집수리', icon: icon4 },
-  {
-    title: '시공전',
-    img: before1,
-  },
-  {
-    title: '시공후',
-    img: after1,
-  },
-];
+  content: [
+    {
+      name: '0.1 현장2',
+      img: pipeProcess1,
+    },
+    {
+      name: '02. 준비2',
+      img: pipeProcess2,
+    },
+    {
+      name: '03. 철거2',
+      img: pipeProcess3,
+    },
+    {
+      name: '04. 배관교체2',
+      img: pipeProcess4,
+    },
+    {
+      name: '05. 미장2',
+      img: pipeProcess5,
+    },
+    {
+      name: '06. 완료2',
+      img: pipeProcess6,
+    },
+  ],
+};
 
-export const questionInfo = [
-  {
-    question: '배관설비가 무엇인가요?',
-    answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+export const leakingInfo = {
+  title: {
+    name: '누수설비',
+    icon: icon3,
   },
-  {
-    question: '비용은 어떻게 되나요?',
-    answer: '방문후 정확한 견적이 가능합니다.',
+  content: [
+    {
+      name: '시공전',
+      img: before1,
+    },
+    {
+      name: '시공후',
+      img: after1,
+    },
+  ],
+};
+
+export const repairsInfo = {
+  title: {
+    name: '집수리',
+    icon: icon4,
   },
-];
+  content: [
+    {
+      name: '시공전',
+      img: before1,
+    },
+    {
+      name: '시공후',
+      img: after1,
+    },
+  ],
+};
+
+export const questionInfo = {
+  pipe: [
+    {
+      question: '배관설비과 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+    {
+      question: '배관설비과 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+  ],
+  heating: [
+    {
+      question: '난방설비과 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+    {
+      question: '배관설비과 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+  ],
+  leaking: [
+    {
+      question: '난방설비과 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+    {
+      question: '배관설비과 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+  ],
+  repairs: [
+    {
+      question: '누수설비는 무엇인가요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+    {
+      question: '집수리는 어떤걸 하나요?',
+      answer: '가정 내 냉온수 공급, 실내 환기, 생활 하수 배출 따위에 필요한 급수 및 배수 설비 입니다. ',
+    },
+  ],
+};
+
+// {
+//   question: '비용은 어떻게 되나요?',
+//   answer: '방문후 정확한 견적이 가능합니다.',
+// },
