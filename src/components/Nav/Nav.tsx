17545b759a1a5604ed6c4bb4f2ae950d28923407
yeongjong310 <tohero21@naver.com>
2021-11-09 18:03:22 +0900

    feat: nav open시에  scroll 금지 로직 제거

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -10,38 +10,25 @@ export default class Nav extends Node {
     return isActive ? style.active : '';
   }
 
-  removeOverflowHidden() {
-    document.body.style.overflow = '';
-  }
-
   template() {
     return (
       <nav class={style.nav}>
         <span class={style.triAngle} aria-hidden="true" role="presentation"></span>
         <ul id="menu" role="menu" aria-labelledby="IDREF">
           <li role="none">
-            <Link
-              onclick={this.removeOverflowHidden.bind(this)}
-              classFunc={this.handleLinkActive.bind(this)}
-              role="menuitem"
-              to="/about"
-            >
+            <Link classFunc={this.handleLinkActive.bind(this)} role="menuitem" to="/about">
               소개
             </Link>
             <img src={arrowSrc} />
           </li>
           <li>
-            <Link
-              onclick={this.removeOverflowHidden.bind(this)}
-              classFunc={this.handleLinkActive.bind(this)}
-              to="/service"
-            >
+            <Link classFunc={this.handleLinkActive.bind(this)} role="menuitem" to="/service">
               서비스
             </Link>
             <img src={arrowSrc} />
           </li>
           <li>
-            <Link onclick={this.removeOverflowHidden.bind(this)} classFunc={this.handleLinkActive.bind(this)} to="/map">
+            <Link classFunc={this.handleLinkActive.bind(this)} role="menuitem" to="/map">
               찾아오시는 길
             </Link>
             <img src={arrowSrc} />
