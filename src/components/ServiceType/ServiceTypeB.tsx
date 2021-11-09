970b4b0dc65a515cd2e8501bdbc9887da68c43d4
SpearJin <87363129+SpearJin@users.noreply.github.com>
2021-11-09 16:02:41 +0900

    fix: 서비스타입B props 타입 수정

--- src/components/ServiceType/ServiceTypeB.tsx
+++ src/components/ServiceType/ServiceTypeB.tsx
@@ -1,28 +1,36 @@
 import { dom } from '@/utils/babel';
 import { Node } from '@/components';
 import styles from './ServiceTypeB.module.scss';
+import { generateClassName } from '@/utils';
 
-interface Props {
-  info: any;
+export interface Props {
+  info: {
+    title: {
+      name: string;
+      icon: string;
+    };
+
+    content: {
+      name: string;
+      img: string;
+    }[];
+  };
 }
 
 /** @jsx dom */
-export default class ServiceTypeB extends Node<Props, State> {
+export default class ServiceTypeB extends Node<Props> {
   template() {
     const { info } = this.props;
     return (
       <div class={styles.typeB}>
-        <img src={info[0].icon} class={styles.title__icon} />
-        <h2>{info[0].title}</h2>
+        <img src={info.title.icon} class={styles.title__icon} />
+        <h2>{info.title.name}</h2>
         <ul class={styles.typeBList}>
-          {info.map(item => {
-            if (item.img === undefined) {
-              return '';
-            }
+          {info.content.map(item => {
             return (
               <li class={styles.typeBList__card}>
-                <img src={item.img} class={styles.typeBList__img} />
-                <span>{item.title}</span>
+                <img src={item.img} class={generateClassName(styles.typeBList__img, styles.before)} />
+                <span>{item.name}</span>
               </li>
             );
           })}
