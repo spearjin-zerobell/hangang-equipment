0ea85191836c5e98a75517c192c9334aea5763c0
yeongjong310 <tohero21@naver.com>
2021-11-09 14:22:17 +0900

    feat: Profile 컴포넌트 구현 및 About 페이지 1차 완료

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as Link } from './Link/Link';
 export { default as Nav } from './Nav/Nav';
 export { default as Header } from './Header/Header';
 export { default as AboutCard } from './About/AboutCard/AboutCard';
+export { default as ProfileCard } from './About/ProfileCard/ProfileCard';
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,12 +1,43 @@
 import { dom } from '@/utils/babel';
-import { Node } from '@/components';
-import { Route, Router } from '@/Router/Router';
+import { Node, AboutCard, ProfileCard } from '@/components';
+import style from './About.module.scss';
+import data from './data';
 
 /** @jsx dom */
 export default class About extends Node<unknown, { type: boolean }> {
-  state = { type: true };
-
   template() {
-    return <div>About</div>;
+    return (
+      <main class={style.main}>
+        <section class={style.section + ' ' + style.backgroundBlue}>
+          <h2>회사 소개</h2>
+        </section>
+        <section class={style.section}>
+          <h2>인사말</h2>
+          <p class={style.p}>
+            1993년에 설립된 종합 전문 설비 업체인 한강 설비는 빠르고 정확한 원인 분석, 철저한 AS로 항상 고객만족에
+            최선을 다해왔습니다. 30년 경력의 축적된 기술로 늘 그래 왔듯 사람을 중심에 두고 회사의 성장을 추구하는 한강
+            설비가 되도록 매진하겠습니다.
+          </p>
+        </section>
+        <section class={style.section + ' ' + style.backgroundGray}>
+          <h2>한강설비 철학</h2>
+          <ul>
+            {data.map(({ title, description, iconClassName, iconTitle }) => (
+              <AboutCard title={title} description={description} iconClassName={iconClassName} iconTitle={iconTitle} />
+            ))}
+          </ul>
+        </section>
+        <section class={style.section}>
+          <h2>직원 소개</h2>
+          <ul>
+            <ProfileCard
+              imgSrc="https://www.taycor.com/wp-content/uploads/img-Michael-Hong-v1.jpg"
+              name="김영종"
+              position="대표"
+            />
+          </ul>
+        </section>
+      </main>
+    );
   }
 }
