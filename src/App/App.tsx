4b5df4ae773be57c08fe9a1fd03cfc9dd21fa089
yeongjong310 <tohero21@naver.com>
2021-11-16 22:34:59 +0900

    feat: index.scss를 통해 한번에 import하는 형식으로 변경

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,8 +3,7 @@ import { Node, Footer } from '@/components';
 import { Router } from '@/Router/Router';
 import 'element-closest-polyfill';
 import '@fortawesome/fontawesome-free/js/all.js';
-import '@/styles/normalize.css';
-import '@/styles/global.css';
+import '@/styles/index.scss';
 
 /** @jsx dom */
 export default class App extends Node {
