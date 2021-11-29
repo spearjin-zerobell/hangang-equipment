adeefa9c8c14273bfab3b4aa3e4e1baf3e983e0f
yeongjong310 <tohero21@naver.com>
2021-11-29 15:18:17 +0900

    feat: 소개페이지 데이터 설명 수정

--- src/pages/About/data.ts
+++ src/pages/About/data.ts
@@ -10,13 +10,14 @@ const data = [
     title: '전문',
     iconClassName: 'fa-user-tie',
     iconTitle: '사람',
-    description: '많은 자격증을 보유하여 여러종류의 시공이 전문적으로 가능하고, 수많은 노하우로 통해 고객님께 ....',
+    description: `검증된 자격증을 보유하고 있으며 다양한 종류의 시공을 전문적으로 서비스하고 있습니다. 30년 동안 쌓아온 노하우와 함께
+    단계별 안정적인 시공이 가능합니다.`,
   },
   {
     title: '장비',
     iconClassName: 'fa-tools',
     iconTitle: '도구',
-    description: `본사는 최고의 서비스를 제공하기 위해 최신장비를 보유하고 있습니다.... `,
+    description: `최고의 서비스를 제공하기 위해 항상 최신화된 장비를 보유하고 있습니다.`,
   },
 ];
 
