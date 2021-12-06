f83bf7a0f414cdfc2c9d67b0c6fdd9215c0d5446
yeongjong310 <tohero21@naver.com>
2021-12-07 01:27:04 +0900

    feat: reflow 최소화를 위해 footer 호출 위치 수정

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -10,7 +10,6 @@ export default class App extends Node {
     return (
       <div>
         <Router />
-        <Footer />
       </div>
     );
   }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -12,8 +12,8 @@ export default class Footer extends Node<Props, State> {
   template() {
     return (
       <footer class={style.copyright}>
-        <span>한강설비 ㅣ 대표 : 박창진 ㅣ 사업자등록번호 : 159-88-01629</span>{' '}
-        <span>경기도 용인시 기흥구 신구로53번길 18</span>
+        <span>한강설비 ㅣ 대표 : 김영종 ㅣ 사업자등록번호 : 000-00-00000</span>{' '}
+        <span>경기도 용인시 기흥구 00로 00번길 18</span>
         <span>
           © 2021 <a href="#">HANGANG Corp., Inc.</a> All rights reserved.
         </span>
