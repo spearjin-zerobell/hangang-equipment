e3cfdcb6a60a6b59e6c4ce17db88511611c94eb9
yeongjong310 <tohero21@naver.com>
2021-12-27 19:33:16 +0900

    feat: 디지안 수정 및 ICON string으로 DOM 생성하여 jsx에 삽입

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,18 +1,24 @@
 import { transJSXtoDOM } from '@/utils/babel';
 import { LandingListCard, Node, Title } from '@/components';
 
-import { generateClassName } from '@/utils';
+import { stringToDOMArray } from '@/utils';
 
 // assets
 import { serviceData } from './data';
 import mainImg from './assets/img/main.jpg';
 import styles from './Landing.module.scss';
 import { icon } from '@fortawesome/fontawesome-svg-core';
-import { faPhoneVolume } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 import { IconLookup } from '@fortawesome/fontawesome-common-types';
 
 /** @jsx transJSXtoDOM */
 export default class Landing extends Node {
+  getIconDOM() {
+    const iconTemplete = icon(faPhoneAlt as IconLookup, {}).html;
+    const $icon = stringToDOMArray(iconTemplete[0])[0] as HTMLElement;
+    return $icon;
+  }
+
   template() {
     return (
       <main class={styles.land}>
@@ -23,9 +29,7 @@ export default class Landing extends Node {
               정직한 견적으로 서비스 하겠습니다."
         ></Title>
         <section class={styles.section}>
-          <div>
-            <h3>서비스</h3>
-          </div>
+          <h3>서비스</h3>
           <ul class={styles.main__serviceList}>
             {serviceData.map(service => {
               return <LandingListCard service={service} />;
@@ -34,15 +38,15 @@ export default class Landing extends Node {
         </section>
 
         <section class={styles.land__contact}>
-          <span class={generateClassName('fas fa-phone-volume', styles.contact__img)}> </span>
           <h2 class={styles.landing__title}>전화 상담</h2>
           <div class={styles.contact__description}>
-            <p class={styles.contact__call}>031-000-0000</p>
-            <p class={styles.contact__call}>010-0000-0000</p>
+            <p class={styles.contact__call}>고객센터: 031-000-0000</p>
+            <p class={styles.contact__call}>대표번호: 010-0000-0000</p>
             <p class={styles.contact__email}>par10915@naver.com</p>
-            <button class={styles.contact__btn}>
-              <i class={generateClassName('fas fa-phone-volume', styles.contact__img)}> </i>연락하기
-            </button>
+            <a href={`tel:${process.env.callNumber}`}>
+              {this.getIconDOM.call(this)}
+              <span>전화상담</span>
+            </a>
           </div>
         </section>
       </main>
